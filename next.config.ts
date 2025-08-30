254fca5e20e395d03fc58f4fec55cab93e3d4dad
Radmehr <23xf1@queensu.ca>
2025-08-30 11:50:03 -0400

    changed config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   output: 'export',
   assetPrefix: '/voiceEmotionVisualizer',
-  // basePath: '/voiceEmotionVisualizer', // Uncomment if you use basePath
+  basePath: '/voiceEmotionVisualizer', // Uncomment if you use basePath
 };
 
 export default nextConfig;
